20458ed613eca8fe0105baade4817d79d3b0d744
aku424tt <61079024+aku424tt@users.noreply.github.com>
2021-01-07 15:21:10 +0900

    added selectors.js file to get states

---
+++ src/reducks/users/selectors.js
@@ -0,0 +1,12 @@
+//このファイルではstateとして扱っている関数を呼び出す処理をまとめて書いている。
+import {createSelector} from 'reselect';
+
+//この定数でstate全体のusersを定義している。
+//stateの引数には呼び出し元の関数の引数の値が入る
+const usersSelector = (state) => state.users;
+
+//getUserIdという関数を定義していて、上で定義したusersSelectorの中からuidを取り出している
+export const getUserId = createSelector(
+    [usersSelector],
+    state =>state.uid
+)
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {push} from "connected-react-router"
+import {getUserId} from "../reducks/users/selectors"
+import { signInAction } from '../reducks/users/actions';
+
 
 const Home = () => {
+    const selector = useSelector(state =>state)
+    const uid = getUserId(selector)
     const dispatch = useDispatch()
     return(
         <>
             <h2>home</h2>
+            <p>{uid}</p>
+            <button onClick = {() => dispatch(signInAction({uid: "0001", username: "takuto"}))}>
+                更新
+            </button>
             <button onClick={() => dispatch(push('/login/'))}>
                 ログイン画面にいく
             </button>
